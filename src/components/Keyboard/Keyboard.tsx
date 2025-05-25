8811b440b63cd69c19cb3409639846ca44d7e430
yume <hoffmenn334@gmail.com>
2025-05-25 11:12:45 -0300

    third commit

--- src/components/Keyboard/Keyboard.tsx
+++ src/components/Keyboard/Keyboard.tsx
@@ -4,16 +4,19 @@ import { useState } from "react";
 import Button from "../Button/Button";
 import Hiragana from "@/data/hiragana/hiragana";
 import Katakana from "@/data/katakana/katakana";
+import Dakuten from "@/data/dakuten/dakuten";
 import styles from "./Keyboard.module.css";
 import translator from "@/functions/translator";
 
 export default function Keyboard() {
   let [romaji, setRomaji] = useState<string>("");
-  const [kana, setKana] = useState<string>("");
-  const [selectedKana, setSelectedKana] = useState<string>('hiragana');
+  let [kana, setKana] = useState<string>("");
+  const [selectedKana, setSelectedKana] = useState<string>("hiragana");
 
   function changeKana(): void {
-    setSelectedKana(selectedKana == 'hiragana' ? 'katakana' : 'hiragana');
+    setSelectedKana(selectedKana == "hiragana" ? "katakana" : "hiragana");
+    setKana("");
+    setRomaji("");
   }
 
   function clearInput(): void {
@@ -21,14 +24,31 @@ export default function Keyboard() {
     setKana("");
   }
 
-  function getKana(event): void {
+  function dakuten(): void {
+    const result: string = Dakuten[(kana += "“")];
+    setKana("");
+    setKana((prevHiragana) => prevHiragana + result);
+  }
+
+  function handakuten(): void {
+    const regex = /[は, ひ, ふ, へ, ほ]/;
+    const regexMatch = kana.match(regex);
+
+    if (regexMatch) {
+      const result: string = Dakuten[(regexMatch[0] += "˚")];
+      setKana("");
+      setKana((prevHiragana) => prevHiragana + kana.replace(regex, result));
+    }
+  }
+
+  function getKana(event: any): void {
     const kana = event.target.textContent;
     setKana((prevHiragana) => prevHiragana + kana);
   }
 
   function getRomaji(): void {
-    const result: string = translator({romaji, kana, p: selectedKana})
-    setRomaji(result)
+    const result: string = translator({ romaji, kana, p: selectedKana });
+    setRomaji(result);
   }
 
   return (
@@ -44,7 +64,7 @@ export default function Keyboard() {
       </div>
 
       <div className={styles.keyboard}>
-        {selectedKana == 'hiragana'
+        {selectedKana == "hiragana"
           ? Hiragana.map((item) => (
               <Button
                 text={item}
@@ -60,18 +80,23 @@ export default function Keyboard() {
               />
             ))}
 
+        <button className={styles.clear} onClick={() => dakuten()}>
+          “
+        </button>
+
+        <button className={styles.clear} onClick={() => handakuten()}>
+          ˚
+        </button>
+
         <button className={styles.clear} onClick={() => clearInput()}>
           Clear
         </button>
 
         <button className={styles.katakana} onClick={() => changeKana()}>
-          Katakana
+          {selectedKana}
         </button>
 
-        <button
-          className={styles.convert}
-          onClick={() => getRomaji()}
-        >
+        <button className={styles.convert} onClick={() => getRomaji()}>
           Convert to romaji
         </button>
       </div>
